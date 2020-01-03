aeb2c2e3c25874848da4dd11774d3c8fac52b6eb
1000mmr <mmagnani4@gmail.com>
2020-01-03 23:02:26 +0100

    Add files via upload

--- kodi-connection/windows.js
+++ kodi-connection/windows.js
@@ -43,7 +43,7 @@ const KodiWindows = [
         { 'TvShow Actors': 'videodb://tvshows/actors/' },
         { 'TvShow Studios': 'videodb://tvshows/studios/' },
         { 'Episodi aggiunti di recente': 'videodb://recentlyaddedepisodes/' },
-        { 'Cerca nuovi Episodi': 'plugin://plugin.video.SODbyHDs/?ewogICAgImFjdGlvbiI6ICJ1cGRhdGVfdmlkZW9saWJyYXJ5IiwKICAgICJjaGFubmVsIjogInZpZGVvbGlicmFyeSIsIAogICAgImZvbGRlciI6ICJmYWxzZSJ9' },
+        { 'Cerca nuovi Episodi': 'plugin://plugin.video.SODbyHDs/?action=update_videolibrary&channel=videolibrary&folder=False' },
         { 'Newest Episodes': 'videodb://recentlyaddedepisodes/' },
         { 'Latest Episodes': 'videodb://recentlyaddedepisodes/' },
         { 'In Progress Tv Shows': 'videodb://inprogresstvshows' },
