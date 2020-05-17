24aea9783c30468f33ac070f63339bdee43c8b86
1000mmr <mmagnani4@gmail.com>
2020-05-17 04:05:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -245,6 +245,8 @@ app.all('/spotyplay', exec(Helper.kodiExecuteAddonSpotyPl));
 
 app.all('/spotyalb', exec(Helper.kodiExecuteAddonSpotyAl));
 
+app.all('/spotytrk', exec(Helper.kodiExecuteAddonSpotyTr));
+
 app.all('/spotysearch', exec(Helper.kodiExecuteAddonSpotySearch));
 
 app.all('/updateepi', exec(Helper.kodiExecuteAddonUpdateEpi));
